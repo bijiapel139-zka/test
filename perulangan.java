5a359e1c403bc4d1011db0df8b5b3f6f2669fe4a
bijiapel139-zka <bijiapel139@gmail.com>
2026-06-09 10:27:53 +0700

    Add files via upload

---
+++ perulangan.java
@@ -0,0 +1,13 @@
+
+public class perulangan {
+    public static void main(String[] args) {
+        int nilaiawal,batas;
+        nilaiawal = 5;
+        batas = 3;
+
+        do {
+            System.out.println("nilaiawal");
+            nilaiawal++;
+        } while (nilaiawal <= batas);
+    }
+}
