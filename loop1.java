943eafbbe58670088f07be979778c535064c9639
bijiapel139-zka <bijiapel139@gmail.com>
2026-06-09 10:27:08 +0700

    Add files via upload

---
+++ loop1.java
@@ -0,0 +1,13 @@
+public class loop1 {
+            public static void main(String[] args) {
+            int nilaiawal,batas;
+            nilaiawal = 0;
+            batas = 10;
+    
+            do {
+                System.out.println("nilaiawal");
+                nilaiawal++;
+            } while (nilaiawal <= batas);
+        }
+    }
+
