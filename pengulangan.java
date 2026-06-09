aa42379f28138133e5943462a1dd70c8ceba35d1
bijiapel139-zka <bijiapel139@gmail.com>
2026-06-09 10:27:26 +0700

    Add files via upload

---
+++ pengulangan.java
@@ -0,0 +1,18 @@
+public class pengulangan {
+    public static void main(String[]args){
+int batas = 5;
+int y = 10;
+
+while (y >=batas) {
+    int x = 1;
+    while (x <= y) {
+        System.out.println("1");
+        x+=1;
+    }
+    y-=1;
+
+        
+    }
+}
+    }
+
